2865953b19f22c5df316c3d6786356317d12a789
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-11-03 12:07:47 +0100

    start setup test for egui rendering

--- tests/opengl3.rs
+++ tests/opengl3.rs
@@ -6,7 +6,7 @@ use std::time::Duration;
 
 use harness::opengl3::Opengl3Harness;
 use hook::HookExample;
-use hudhook::hooks::opengl3::ImguiOpenGl3Hooks;
+use hudhook::hooks::opengl3::{EguiOpenGl3Hooks, ImguiOpenGl3Hooks};
 use hudhook::*;
 use tracing::metadata::LevelFilter;
 
@@ -25,6 +25,29 @@ fn test_imgui_opengl3() {
 
     if let Err(e) =
         Hudhook::builder().with(HookExample::new().into_hook::<ImguiOpenGl3Hooks>()).build().apply()
+    {
+        eprintln!("Couldn't apply hooks: {e:?}");
+    }
+
+    thread::sleep(Duration::from_millis(5000));
+    drop(opengl3_harness);
+}
+
+#[test]
+fn test_egui_opengl3() {
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::TRACE)
+        .with_thread_ids(true)
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_names(true)
+        .init();
+
+    let opengl3_harness = Opengl3Harness::new("OpenGL3 hook example");
+    thread::sleep(Duration::from_millis(500));
+
+    if let Err(e) =
+        Hudhook::builder().with(HookExample::new().into_hook::<EguiOpenGl3Hooks>()).build().apply()
     {
         eprintln!("Couldn't apply hooks: {e:?}");
     }
